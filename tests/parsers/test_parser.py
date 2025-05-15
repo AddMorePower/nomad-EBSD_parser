4f363b97930d8c93ecfc6d028af5cd519a057234
Guillaume Gaisne <guillaume.gaisne@esrf.fr>
2025-05-15 11:13:45 +0200

    Make parsing unit test pass

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -8,6 +8,8 @@ from nomad_ebsd_parser.parsers.parser import NewParser
 def test_parse_file():
     parser = NewParser()
     archive = EntryArchive()
-    parser.parse('tests/data/example.out', archive, logging.getLogger())
+    parser.parse(
+        'tests/data/Cu2160-2000_Site_1_Map_Data_9.cpr', archive, logging.getLogger()
+    )
 
-    assert archive.workflow2.name == 'test'
+    assert archive.data.format_version == '5.0'
